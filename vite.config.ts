b55f3f24878676a2a650d9d46387d5ce4c03e826
Vecheslav-Eremeev <eremeev-vecheslav@yandex.ru>
2024-09-02 12:12:02 +0500

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'pproduct-management'
+  base: 'product-management'
 })
 
